0c44292b0c740dd505feb12707173aec4a4df978
Roque <roquejr@celepar.pr.gov.br>
2022-08-09 16:29:56 -0300

    Teste para o primeiro commit de informações

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false ,
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Transferências"),
+          title: Text("Transferências atuais"),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {  },
