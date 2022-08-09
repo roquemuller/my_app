3cc9272a687082562ad793c422bea9c42e593b9e
Roque Junior <roquejunior@gmail.com>
2022-08-09 09:43:00 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false ,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Transferências"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+          child: Icon(Icons.add),
+        ),
+      ),
+    ));
+
